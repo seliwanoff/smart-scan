f6de17d2da38889eb88ff90c90b6fe6a283d5737
seliwanoff <olagunjuselim2019@gmail.com>
2026-02-25 10:35:46 +0100

    fix

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,11 +1,12 @@
 import { Client, Account, Storage, Databases } from "appwrite";
-export const baseURL = import.meta.env.VITE_APPWRITE_ENDPOINT;
-export const projectID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 
-export const client = new Client()
-  .setEndpoint(baseURL)
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+const client = new Client();
+
+client
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT!) // https://cloud.appwrite.io/v1
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID!)
+  .setDevKey(import.meta.env.VITE_APPWRITE_API_KEY!); // <-- add your API key here
+
 export const account = new Account(client);
 export const storage = new Storage(client);
-
-export const databases = new Databases(client);
+export const db = new Databases(client);
