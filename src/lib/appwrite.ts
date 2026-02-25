d03e020353735fc6aa7bf2383d631452d2bd3991
seliwanoff <olagunjuselim2019@gmail.com>
2026-02-25 10:00:20 +0100

    fix

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -3,8 +3,8 @@ export const baseURL = import.meta.env.VITE_APPWRITE_ENDPOINT;
 export const projectID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 
 export const client = new Client()
-  .setEndpoint(baseURL) // your Appwrite endpoint
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID); // replace with your project ID
+  .setEndpoint(baseURL)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 export const account = new Account(client);
 export const storage = new Storage(client);
 
